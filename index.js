75709b70f9ae515294d202a989c46e5360552ac4
michael2023-code <michael.mutunga@student.moringaschool.com>
2023-07-17 19:21:41 +0300

    push changes

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+     function writeCards(names, message){
+     const messages = [];
+
+     for(n=0; n<names.length; n++){
+      messages.push("Thank you, ${names[n]}, for the wonderful ${message} gifts!")
+     }
+     return messages;
+   }
+   console.log(writeCards(["Mike", "Jose", "Eric"], wedding))
+
+   function countDown(number){
+      while(number>=0){
+         console.log(number);
+         number --;
+      }
+   }
